564c4a0c49463b70e915a94a68e76f0f10284e61
pablosalina25 <pablosalina25@gmail.com>
2024-01-12 11:14:19 -0800

    fixed bug

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,39 +8,39 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i = 0, min_index = 0, p, temp, swap_occurred = 0;
+    size_t i = 0, min_index = 0, p, temp, swap_occurred = 0;
 
-	if (!array)
-	return;
+    if (!array)
+        return;
 
-	while (i < (size - 1))
-	{
-	min_index = i;
-	p = i + 1;
+    while (i < (size - 1))
+    {
+        min_index = i;
+        p = i + 1;
 
-	while (p < size)
-	{
-	if (array[min_index] > array[p])
-	min_index = p;
-	p++;
-	}
+        while (p < size)
+        {
+            if (array[min_index] > array[p])
+                min_index = p;
+            p++;
+        }
 
-	if (min_index != i)
-	{
-	temp = array[i];
-	array[i] = array[min_index];
-	array[min_index] = temp;
-	print_array(array, size);
-	swap_occurred = 1;
-	}
-	else
-	{
-	swap_occurred = 0;
-	}
+        if (min_index != i)
+        {
+            temp = array[i];
+            array[i] = array[min_index];
+            array[min_index] = temp;
+            print_array(array, size);
+            swap_occurred = 1;
+        }
+        else
+        {
+            swap_occurred = 0;
+        }
 
-	i++;
+        i++;
 
-	if (swap_occurred == 0)
-	break;
-	}
+        if (swap_occurred == 0)
+            break;
+    }
 }
--- print_array.c
+++ print_array.c
@@ -1,24 +1,21 @@
 #include "sort.h"
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
  * print_array - Prints an array of integers
- *
  * @array: The array to be printed
- * @size: Number of elements in @array
+ * @size: Number of elements in the array
  */
 void print_array(const int *array, size_t size)
 {
     size_t i;
 
-    i = 0;
-    while (array && i < size)
+    if (!array || size == 0)
+        return;
+
+    printf("%d", array[0]);
+    for (i = 1; i < size; i++)
     {
-        if (i > 0)
-            printf(", ");
-        printf("%d", array[i]);
-        ++i;
+        printf(", %d", array[i]);
     }
     printf("\n");
 }
