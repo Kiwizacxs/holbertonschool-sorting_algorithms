b6be72939da95bd8ad37bee90417fdcb6bdab1bc
pablosalina25 <pablosalina25@gmail.com>
2024-01-12 11:36:19 -0800

    betty style fixed

--- print_array.c
+++ print_array.c
@@ -7,18 +7,18 @@
  */
 void print_array(const int *array, size_t size)
 {
-    size_t i;
+	size_t i;
 
-    if (!array || size == 0)
-    {
-        printf("\n");
-        return;
-    }
+	if (!array || size == 0)
+	{
+	printf("\n");
+	return;
+	}
 
-    printf("%d", array[0]);
-    for (i = 1; i < size; i++)
-    {
-        printf(", %d", array[i]);
-    }
-    printf("\n");
+	printf("%d", array[0]);
+	for (i = 1; i < size; i++)
+	{
+	printf(", %d", array[i]);
+	}
+	printf("\n");
 }
