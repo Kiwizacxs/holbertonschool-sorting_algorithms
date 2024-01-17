38e6dcc583dbf43b22632c3cd7c0534398c9c097
Kiwizacxs <brian.deleon2016@outlook.com>
2024-01-17 02:27:06 +0000

    bubble_sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -4,22 +4,35 @@ void
 bubble_sort(int *array, size_t size)
 {
 	size_t n;
-	int aux;
+	int aux = 0;
 
-	while (sorted(array) != -1)
+	while (aux != -1)
 	{
-		for (n = 0; n < size; n++);
+		for (n = 0; n < size - 1; n++)
 		{
-			if (array[n] > array[n + 1])
-		}
-		for (n = 0; n < size; n++)
-		{
-			if (array[n] < array[n + 1])
+			if (array[n] > array[n + 1] && n + 1 != size)
 			{
 				aux = array[n];
 				array[n] = array[n + 1];
 				array[n + 1] = aux;
+				print_array(array, size);
 			}
 		}
+		aux = sorted(array, size);
 	}
+}
+
+int
+sorted(int *array, size_t size)
+{
+	size_t n = 0;
+
+	while (n != size && n + 1 != size)
+	{
+		if (array[n] > array[n + 1])
+			return (0);
+		n++;
+
+	}
+	return (-1);
 }
